0085e71613a2e2cbbc9f74004aa78a7c6c972178
XYYaua <2690833249@qq.com>
2023-08-08 13:08:44 +0800

    Site updated: 2023-08-08 13:08:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["post/2023/hello-world/","post/2023/post/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["post/2023/hello-world/","post/2023/zdsny/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
