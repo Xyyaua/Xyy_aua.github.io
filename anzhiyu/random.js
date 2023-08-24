8ac4d82d8fb1c5bd707b270b5990f7db61eb2661
XYYaua <2690833249@qq.com>
2023-08-25 01:58:44 +0800

    Site updated: 2023-08-25 01:58:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["post/2023/hello-world/","post/2023/zdsny/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["post/2023/zdsny/","post/2023/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
