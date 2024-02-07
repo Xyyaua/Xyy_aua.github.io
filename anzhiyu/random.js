e8dc14779ce3c319b1f305fd7b396a4e37740b0e
Xyyaua <2690833249@qq.com>
2024-02-08 01:01:41 +0800

    Site updated: 2024-02-08 01:01:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/2023/hello-world/","post/2023/zdsny/","post/2023/srbgm/"];function toRandomPost(){
+var posts=["post/2023/hello-world/","post/2023/srbgm/","post/2023/zdsny/","post/2024/lc/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
