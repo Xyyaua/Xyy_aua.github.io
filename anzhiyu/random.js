2c452a67c612ac5b46c5d92d4758a28f1e67915e
Xyyaua <2690833249@qq.com>
2024-02-15 20:55:53 +0800

    Site updated: 2024-02-15 20:55:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/2023/hello-world/","post/2024/lc/","post/2023/srbgm/","post/2024/srdfq/","post/2023/zdsny/"];function toRandomPost(){
+var posts=["post/2023/hello-world/","post/2023/srbgm/","post/2024/lc/","post/2024/srdfq/","post/2023/zdsny/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
